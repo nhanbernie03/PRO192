c058807e1d8e1d687954fb60af6b562f5179abe0
Your Name <you@example.com>
2023-09-21 14:46:51 +0700

    Project

--- Bassic/LearnMethod/Const.java
+++
@@ -1,9 +0,0 @@
-package LearnMethod;
-
-public class Const { // hằng số
-    public static void main(String[] args) {
-        final int i = 10; // const
-        System.out.println("Final i = "+ i);
-        // i = 45; // i can't be assigned ======> Wrong
-    }
-}
